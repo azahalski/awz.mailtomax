f7fcc90a060e5d5d555fdcc0eae81d8e4cfc8dca
Andrew Zahalski <andrew@zahalski.dev>
2026-05-23 16:35:27 +0300

    gulp checkpoint

---
+++ update/1.0.2/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "1.0.2",
+	"VERSION_DATE" => "2026-05-23 14:38:00"
+);
+?>
---
+++ update/1.0.2/lang/ru/options.php
@@ -0,0 +1,18 @@
+<?php
+$MESS['AWZ_MAILTOMAX_OPT_TITLE'] = "AWZ: Письма в Макс";
+$MESS['AWZ_MAILTOMAX_OPT_L_BTN_SAVE'] = "Сохранить настройки";
+$MESS['AWZ_MAILTOMAX_OPT_SECT2'] = "Права доступа";
+$MESS['AWZ_MAILTOMAX_OPT_SECT1'] = "Общие настройки";
+$MESS['AWZ_MAILTOMAX_OPT_SHOW_DESC'] = "Отметьте почтовые шаблоны, которые следует пересылать в Макс";
+$MESS['AWZ_MAILTOMAX_OPT_SHOW_DESC_1'] = "Код сайта";
+$MESS['AWZ_MAILTOMAX_OPT_SHOW_DESC_2'] = "Тип шаблона";
+$MESS['AWZ_MAILTOMAX_OPT_SHOW_DESC_3'] = "Тема письма";
+$MESS['AWZ_MAILTOMAX_OPT_SHOW_DESC_4'] = "Отправлять в Макс";
+$MESS['AWZ_MAILTOMAX_OPT_SHOW_DESC_5'] = "Запрет отправки письма";
+$MESS['AWZ_MAILTOMAX_OPT_TGKEY'] = "Токен доступа бота Макс";
+$MESS['AWZ_MAILTOMAX_OPT_TGID'] = "ID пользователя, канала или группы";
+$MESS['AWZ_MAILTOMAX_OPT_DISABLED'] = "Отключить модуль";
+$MESS['AWZ_MAILTOMAX_OPT_TGID_AUTO_OK'] = "1) Перейдите по ссылке и авторизуйтесь.<br>2) после авторизации войдите в чат и напишите любое сообщение<br> 3) В браузере в адресной строке увидите ид чата, который и введите в поле ниже";
+$MESS['AWZ_MAILTOMAX_OPT_TGID_AUTO'] = "По данному токену бот не найден";
+$MESS['AWZ_MAILTOMAX_OPT_TGID_NO'] = "Введите токен выше и нажмите кнопку сохранить";
+$MESS['AWZ_MAILTOMAX_HOOK_IS_SET'] = "Хук уже существует и отличается от текущего сайта, автоматическая установка ид чата невозможна";
